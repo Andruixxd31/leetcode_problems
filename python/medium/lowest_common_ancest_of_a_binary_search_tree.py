2145065379991661306d83dace580260c2e9edcb
adnruixxd31 <andruix93@gmail.com>
2023-08-14 09:49:35 -0600

    new solution

--- python/medium/lowest_common_ancest_of_a_binary_search_tree.py
+++ python/medium/lowest_common_ancest_of_a_binary_search_tree.py
@@ -7,22 +7,10 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or not p or not q: return None
-        
-        def search(root, p, q):
-            if not root: return False
-            mid = left = right = False
-            if root.val == p.val or root.val == q.val: 
-                mid = True
-            
-            left = search(root.left, p, q)
-            right = search(root.right, p, q)
-            if mid:
-                if left or right:
-                    self.res = root
-            elif left and right:
-                self.res = root
-            return mid or left or right
-            
-        search(root, p, q)
-        return self.res
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
