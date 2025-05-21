7e10032e5d903bf237e997b1432d7fa77b992dba
Andres <andruix93@gmail.com>
2025-05-21 15:05:10 -0600

    solved

---
+++ python/medium/odd-even-linked-list.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+        odd = head
+        even = head.next
+        ehead = even
+        while even != None and even.next != None:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = ehead
+        return head
